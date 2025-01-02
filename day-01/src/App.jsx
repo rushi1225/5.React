b66e39c72eb066d88f8332f8331705de0bb909d6
rushi1225 <rushibhingare25@gmail.com>
2025-01-02 15:01:26 +0530

    day-01 react

--- day-01/src/App.jsx
+++ day-01/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Rushi from "./components/Rushi";
+import TestComponent2 from "./components/TestComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TestComponent></TestComponent>
+      <h1>Rushikesh</h1>
+      {/* adding from TestComponent file */}
+      <TestComponent2></TestComponent2>
+      <Rushi></Rushi>
     </>
-  )
+  );
 }
 
-export default App
+// creating first component
+//component always start with Capital letter
+
+function TestComponent() {
+  return <h2 className="test1">Hello this is test component 1</h2>;
+}
+
+export default App;
