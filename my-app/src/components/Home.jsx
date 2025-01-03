c01186138080743f7a0093c017e69cba6185a319
rushi1225 <rushibhingare25@gmail.com>
2025-01-03 14:53:38 +0530

    day-02 react

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
                                         alt="..." />
                                     <div className="carousel-caption d-none d-md-block text-dark">
                                         <h1>Welcome This Theme</h1>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        <p>Lorem ipsum dolorteh sit amet, consectetur adipisicing elit, sed do eiusmod
                                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                                             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                                             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
